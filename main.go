64b5d4248e1d427b888bcbc29466270eafb9b239
Margulan Moldabekov <moldabekov@gmail.com>
2018-01-16 19:10:27 +0600

    waiting mode implemented

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"os"
-	"crypto/sha256"
+	"time"
 
-	"github.com/moldabekov/termux-virustotal/vt"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"github.com/fatih/color"
+	"github.com/moldabekov/termux-virustotal/vt"
+	"github.com/moldabekov/spinner"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 const apiurl = "https://www.virustotal.com/vtapi/v2/"
@@ -19,12 +21,12 @@ var (
 	filescan  = app.Command("file", "File scan mode")
 	filename  = filescan.Arg("FILE", "File to scan").Required().String()
 	forceFile = filescan.Flag("force", "rescan file").Bool()
-	waitFile = filescan.Flag("wait","wait for results").Bool()
+	waitFile  = filescan.Flag("wait", "wait for results").Bool()
 
 	urlscan  = app.Command("url", "URL scan mode")
 	urlname  = urlscan.Arg("URL", "URL to scan").Required().String()
 	forceUrl = urlscan.Flag("force", "rescan URL").Bool()
-	waitUrl = urlscan.Flag("wait","wait for results").Bool()
+	waitUrl  = urlscan.Flag("wait", "wait for results").Bool()
 )
 
 func check(err error) {
@@ -32,6 +34,13 @@ func check(err error) {
 		fmt.Printf("FATAL: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func loader(s string) {
+	spin := spinner.New("%s " + s)
+	spin.Start()
+	defer spin.Stop()
+	time.Sleep(60 * time.Second)
 }
 
 func sha256sum(filename string) string {
@@ -50,25 +59,8 @@ func sha256sum(filename string) string {
 
 func printFileResult(result *govt.FileReport) {
 	color.Set(color.FgHiYellow)
-	fmt.Printf("%s file was already scanned.\n", *filename)
+	fmt.Printf("%s file scan results:\n", *filename)
 	fmt.Printf("sha256 hashsum: %s\n\n", result.Sha256)
-	color.Set(color.FgHiCyan)
-	fmt.Printf("Detection ratio: %v/%v\n\n", result.Positives, result.Total)
-	for i := range result.Scans {
-		if result.Scans[i].Detected {
-			color.Set(color.FgHiRed, color.Bold)
-			fmt.Printf("AV: %s\nResult: %s\n\n", i, result.Scans[i].Result)
-		} else {
-			color.Set(color.FgHiGreen, color.Bold)
-			fmt.Printf("AV: %s\nDetected: %t\n\n", i, result.Scans[i].Detected)
-		}
-	}
-	os.Exit(0)
-}
-
-func printUrlResult(result *govt.UrlReport) {
-	color.Set(color.FgHiYellow)
-	fmt.Printf("%s was already scanned.\n", *urlname)
 	color.Set(color.FgHiCyan)
 	fmt.Printf("Detection ratio: %v/%v\n\n", result.Positives, result.Total)
 	for i := range result.Scans {
@@ -94,17 +86,46 @@ func scanFile(filename string) {
 		r, err := vt.GetFileReport(sha256sum(filename))
 		check(err)
 
-		// If file was previously checked give results
+		// If file was previously scanned print results
 		if r.Status.ResponseCode != 0 {
 			printFileResult(r)
 		}
 	}
 
 	// Scan file
-	report, err := vt.ScanFile(filename)
-	check(err)
+	if !*waitFile {
+		report, err := vt.ScanFile(filename)
+		check(err)
+		fmt.Printf("%s\n", report.Status.VerboseMsg)
+	} else { // Wait for results if user wishes
+		for m := 0; m <= 10; m++ {
+			loader(fmt.Sprintf("waiting for results for %d minutes", m))
+			r, err := vt.GetFileReport(sha256sum(filename))
+			check(err)
+			if r.Status.ResponseCode != 0 {
+				printFileResult(r)
+			}
+		}
+		fmt.Printf("\nSomething went wrong with VirusTotal. Maybe their servers are overloaded.\n" +
+			" Please try again later\n")
+	}
+}
 
-	fmt.Printf("%s\n", report.Status)
+func printUrlResult(result *govt.UrlReport) {
+	color.Set(color.FgHiYellow)
+	fmt.Printf("%s scan results:\n\n", *urlname)
+	color.Set(color.FgHiCyan)
+	fmt.Printf("Detection ratio: %v/%v\n\n", result.Positives, result.Total)
+	for i := range result.Scans {
+		if result.Scans[i].Detected {
+			color.Set(color.FgHiRed, color.Bold)
+			fmt.Printf("AV: %s\nResult: %s\n\n", i, result.Scans[i].Result)
+		} else {
+			color.Set(color.FgHiGreen, color.Bold)
+			fmt.Printf("AV: %s\nDetected: %t\n\n", i, result.Scans[i].Detected)
+		}
+	}
+	os.Exit(0)
 }
 
 func scanUrl(urlname string) {
@@ -118,17 +139,29 @@ func scanUrl(urlname string) {
 		r, err := vt.GetUrlReport(urlname)
 		check(err)
 
-		// If file was previously checked give results
+		// If file was previously scanned print results
 		if r.Status.ResponseCode != 0 {
 			printUrlResult(r)
 		}
 	}
 
 	// Scan URL
-	report, err := vt.ScanUrl(urlname)
-	check(err)
-
-	fmt.Printf("%s\n", report.Status)
+	if !*waitUrl {
+		report, err := vt.ScanUrl(urlname)
+		check(err)
+		fmt.Printf("%s\n", report.Status.VerboseMsg)
+	} else { // Wait for results if user wishes
+		for m:=0; m<=10; m++ {
+			loader(fmt.Sprintf("waiting for results for %d minutes", m))
+			r, err := vt.GetUrlReport(urlname)
+			check(err)
+			if r.Status.ResponseCode != 0 {
+				printUrlResult(r)
+			}
+		}
+		fmt.Printf("\nSomething went wrong with VirusTotal. Maybe their servers are overloaded.\n" +
+			" Please try again later\n")
+	}
 }
 
 func main() {
