42c94a320c385f9fa3fdc0d33033ea9a5e5b3654
Margulan Moldabekov <moldabekov@gmail.com>
2018-01-20 21:35:35 +0600

    Bug fixes. Thanks to A. Gurin for bugreporting

--- hashsearch.go
+++ hashsearch.go
@@ -17,21 +17,27 @@ func searchHash(hash string) {
 	r, err := vt.GetFileReport(hash)
 	check(err)
 
-	if r.ResponseCode == 0 {
+	if r.ResponseCode == 0 { // Hash not found
 		color.Set(color.FgHiRed, color.Bold)
 		fmt.Printf("Given hash isn't recognized by VirusTotal\n")
 		color.Unset()
 		os.Exit(1)
 	}
-	if r.Positives > 0 {
-		color.Set(color.FgHiRed)
+	if r.ResponseCode == -2 { // File scan with given hash is still in progress
+		color.Set(color.FgHiYellow)
+		fmt.Printf("\nScan with given hash is still in progress\n", r.Positives, r.Total)
+		color.Unset()
+		os.Exit(1)
+	}
+	if r.Positives > 0 { // Malware detected
 		if !*jsonHash {
+			color.Set(color.FgHiRed, color.Bold)
 			fmt.Printf("\nGiven hash is KNOWN by VirusTotal and has positive results [%d/%d]\n", r.Positives, r.Total)
+			color.Unset()
 		}
 		printFileResult(r)
-		color.Unset()
-	} else {
-		color.Set(color.FgHiGreen)
+	} else { // Malware undetected
+		color.Set(color.FgHiGreen, color.Bold)
 		fmt.Printf("\nGiven hash is KNOWN by VirusTotal and has no positive results\n")
 		color.Unset()
 	}
